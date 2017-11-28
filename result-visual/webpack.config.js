959dca2bce648192f1fabf50ef71882c5643efe9
Erick Guan <fantasticfears@gmail.com>
2017-11-28 17:01:03 +0100

    set up webpack

--- result-visual/webpack.config.js
+++ result-visual/webpack.config.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   entry : './index.js',
-  output : {filename : 'bundle.js', path : path.resolve(__dirname, 'public')}
+  output : {filename : 'bundle.js', path : path.resolve(__dirname, 'public/js')}
 };
